53d3e00701c1d30e2f02ac0e6a294602fe68280d
wky0527 <805245271@qq.com>
2021-11-16 15:54:15 +0800

    build

---
+++ dist/assets/index.49ffe51f.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.91c7083f.js";const t=document.createElement("script");t.src="//at.alicdn.com/t/font_2922939_z6u1ns1w5f.js";document.body.appendChild(t);const r=({type:n})=>e.createElement("svg",{"aria-hidden":"true",className:"adm-icon"},e.createElement("use",{xlinkHref:`#${n}`}));export{r as I};
