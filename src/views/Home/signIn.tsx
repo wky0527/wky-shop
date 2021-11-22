57e038d0b1b88eaf7ea098ad4d811f13c19c982b
wky0527 <805245271@qq.com>
2021-11-22 17:41:29 +0800

    wip: 分享领券+会员专享页面

--- src/views/Home/signIn.tsx
+++ src/views/Home/signIn.tsx
@@ -2,14 +2,25 @@ import React,{FC} from "react";
 import Header from "../../components/Header";
 import { SearchOutline } from 'antd-mobile-icons';
 import Found from '../Found/index';
+import {Image,Toast} from 'antd-mobile';
 import './index.scss';
+import {getImageUrl} from "../../utils";
+
 const SignIn:FC = () => {
     const icon = <SearchOutline />
     return (
         <div className='sign-in-wrap'>
             <Header title='签到领豆' rightTitle={icon}/>
-           <div className='sign-in-image'>
-               <img src="https://img0.baidu.com/it/u=4286852716,36948324&fm=26&fmt=auto" alt=""/>
+           <div className='sign-in-image'  onClick={() =>
+               Toast.show({
+                   icon: 'success',
+                   content: '签到成功！',
+                   afterClose: () => {
+                       console.log('签到成功')
+                   },
+               })
+           }>
+               <Image src={getImageUrl('sign.png')} width={100} height={100}/>
            </div>
            <div className='sign-in-found'>
                <Found />
