16ce91e22aef3d9c822a28ae867b7d6624feecff
wky0527 <805245271@qq.com>
2021-11-26 17:39:36 +0800

    Updates

--- assets/home.60ff446f.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./axios.8c34e798.js";const a={};a.all=(o={})=>t.get("http://localhost:3002/backend/home/all",{params:o});export{a as h};
---
+++ assets/home.a231cd76.js
@@ -0,0 +1,1 @@
+import{i as t}from"./axios.8c34e798.js";const a={};a.all=(e={})=>t.get("http://10.1.3.15:3002/backend/home/all",{params:e});export{a as h};
